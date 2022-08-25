411337c830e1844f44fd29fb18246040f3c773cd
Dmitriy Vasilev <dmitrii.v@railsmuffin.com>
2022-08-25 11:35:02 +0500

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+console.log(null == 0)
+
+const someFunc = () => {
+  return 'haha'
+}
+
+console.log(someFunc())
+
+// import express from 'express'
+
+// const port = process.env.PORT || 3000
+// const app = express()
+
+// app.listen(port, () => {
+//   console.log(`port:${port}`)
+// })
